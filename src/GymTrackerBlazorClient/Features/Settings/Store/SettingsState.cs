b2867ca725dd2fc2f75977e5fc47bbac8d12adb8
Ross Scott <ross.d.scott@gmail.com>
2023-05-30 21:10:57 +0100

    init settings

---
+++ src/GymTrackerBlazorClient/Features/Settings/Store/SettingsState.cs
@@ -0,0 +1,9 @@
+﻿using Fluxor;
+
+namespace GymTracker.BlazorClient.Features.Settings.Store;
+
+[FeatureState]
+public record SettingsState
+{
+    public string AzureBlobBackupContainerSASURI { get; init; }
+}
