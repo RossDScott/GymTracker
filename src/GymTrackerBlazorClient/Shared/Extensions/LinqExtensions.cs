6763ef5a32e540c34d98013cb96176a84cec5d69
Ross Scott <ross.d.scott@gmail.com>
2023-08-12 20:26:03 +0100

    Reorder icons

---
+++ src/GymTrackerBlazorClient/Shared/Extensions/LinqExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace GymTracker.BlazorClient.Shared.Extensions;
+
+public static class LinqExtensions
+{
+    public static IEnumerable<(T item, int index)> LoopIndex<T>(this IEnumerable<T> self) 
+        => self.Select((item, index) => (item, index));
+}
